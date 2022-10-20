ef76f75f9254a84a1481bf8285d1d4d8047808fb
Nuratmanianina <nuratmania166@gmail.com>
2022-10-20 17:47:46 +0800

    perubahan pertama

---
+++ application/controllers/home.php
@@ -0,0 +1,15 @@
+<?php
+
+class home extends CI_Controller{
+  
+  public  function rumah()
+  {
+    echo 'Rumah Ku';
+  } 
+
+
+  public function tampil()
+{
+  return $this->load->view('v_tampil');
+}
+}
